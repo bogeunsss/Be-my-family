6482357fd907d23d1e5d2e74e331732a6c2914aa
김민주 <ynjq01910@naver.com>
2020-08-03 20:26:21 +0900

    [추가] S03P13B201-16 [+] Add Survey model

---
+++ backend/src/main/java/com/web/blog/controller/care/SurveyController.java
@@ -0,0 +1,36 @@
+package com.web.blog.controller.care;
+
+import java.util.List;
+import java.util.Optional;
+
+import com.web.blog.dao.care.CareDao;
+import com.web.blog.model.BasicResponse;
+import com.web.blog.model.care.Care;
+import com.web.blog.model.care.Careboard;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
+        @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
+        @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
+        @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
+
+@CrossOrigin(origins = { "http://localhost:3000" })
+@RestController
+
+public SurveyController CareController {
+}
---
+++ backend/src/main/java/com/web/blog/model/care/Survey.java
@@ -0,0 +1,52 @@
+package com.web.blog.model.care;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
+
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+public class Survey {
+    
+    @Id
+    private int surveyno;
+
+    @JsonIgnore
+	private String uid;
+	private String name;
+	private String email;
+	private String phone;
+	private String job;
+	private int marriaged;
+	private int sex;
+	private Date birthdate;
+	private String nation;
+	private String sido;
+	private String gugun;
+	private String place;
+	private int beforeover;
+	private int beforeadopt;
+	private int presentanimal;
+	private int familyunder;
+	private int familymiddle;
+	private int familyagree;
+	private int dissolution;
+	private int sickness;
+	private int alone;
+	private String temp;
+	private String house;
+	private int eatmoney;
+	private int caremoney;
+	private String reason;
+	private String think;
+
+}
