d92b2268275dfa87f10e1b475c01e0d207ff2a31
최보근 <bogun11904@naver.com>
2020-08-03 19:50:46 +0900

    [수정] Detail.vue [+] 입양신청버튼 수정

--- frontend/src/page/post/Detail.vue
+++ frontend/src/page/post/Detail.vue
@@ -60,17 +60,92 @@
       </v-btn>
       <v-btn
         color="primary"
-        depressed
+        class="ma-2"
+        dark
+        @click="dialog = true"
       >
-        입양할래여~
+        입양신청
       </v-btn>
+      <v-dialog
+        v-model="dialog"
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            입양신청서
+          </v-card-title>
+          <v-card-text>
+            <v-btn
+              color="primary"
+              dark
+              @click="dialog2 = !dialog2"
+            >
+              Open Dialog 3
+            </v-btn>
+          </v-card-text>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
+      <v-dialog
+        v-model="dialog2"
+        max-width="500px"
+      >
+        <v-card>
+          <v-card-title>
+            <span>Dialog 3</span>
+            <v-spacer></v-spacer>
+            <v-menu
+              bottom
+              left
+            >
+              <template v-slot:activator="{ on, attrs }">
+                <v-btn
+                  icon
+                  v-bind="attrs"
+                  v-on="on"
+                >
+                  <v-icon>mdi-dots-vertical</v-icon>
+                </v-btn>
+              </template>
+              <v-list>
+                <v-list-item
+                  v-for="(item, i) in items"
+                  :key="i"
+                  @click="() => {}"
+                >
+                  <v-list-item-title>{{ item.title }}</v-list-item-title>
+                </v-list-item>
+              </v-list>
+            </v-menu>
+          </v-card-title>
+          <v-card-actions>
+            <v-btn
+              color="primary"
+              text
+              @click="dialog2 = false"
+            >
+              Close
+            </v-btn>
+          </v-card-actions>
+        </v-card>
+      </v-dialog>
     </v-card-actions>
+
   </v-card>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+import { mapState, mapActions } from 'vuex'
 
 export default {
     name:'detail',
@@ -104,14 +179,40 @@ export default {
             specialmark: '',
             totalcount: '',
             weight: '',
-          }
+          },
+          dialog: false,
+          dialog2: false,
+          items: [
+            {
+              title: 'Click Me',
+            },
+            {
+              title: 'Click Me',
+            },
+            {
+              title: 'Click Me',
+            },
+            {
+              title: 'Click Me 2',
+            },
+          ],
+          select: [
+            { text: 'State 1' },
+            { text: 'State 2' },
+            { text: 'State 3' },
+            { text: 'State 4' },
+            { text: 'State 5' },
+            { text: 'State 6' },
+            { text: 'State 7' },
+          ],
         }
     },
     computed:{
-
+      ...mapState(['profileData'])
     },
     created(){
-      console.log(this.$route.params)
+      var token = this.$cookies.get('auth-token')
+      this.find(token.email)
       axios.get(`http://localhost:8080/care/detail?desertionNo=${this.$route.params.desertionno}`)
           .then( response => {
               // this.dogs = response.data.message
@@ -121,6 +222,9 @@ export default {
           .catch( error => {
               console.log(error)
           })
+    },
+    methods:{
+      ...mapActions(['find'])
     }
 }
 </script>
