ef73941ad9e5f8c956613b96a949fb9909a50471
최보근 <bogun11904@naver.com>
2020-08-06 19:51:16 +0900

    [수정]S03P13B201-28[+]list 및 Detail페이지

--- frontend/src/page/lost/LostDetail.vue
+++ frontend/src/page/lost/LostDetail.vue
@@ -3,22 +3,35 @@
       <v-container class="pa-0">
           <h1>상세페이지</h1>
           <v-card>
-            <v-card-title> ㅎㅇㅎㅇ </v-card-title>
-            <v-card-subtitle>
+            <v-toolbar flat color="pink" dark>
+                <v-toolbar-title>ㅎㅇㅎㅇ</v-toolbar-title>
+            </v-toolbar>
+            <v-card-subtitle class="mx-auto" style="width: 60%">
                 <v-img
                 src="http://www.animal.go.kr/files/shelter/2014/02/201403010903285_s.jpg"
                 class="white--text align-end"
                 gradient="to bottom, rgba(0,0,0,.1), rgba(0,0,0,.5)"
-                height="500px"
+                max-height="100%"
+                height="auto"
                 >
                 </v-img>
             </v-card-subtitle>
             <v-card-text>
-                ㅎㅇㅎㅇ
+                <v-list v-for="(subTitle, i) in subTitles" :key="i" dense>
+                    <v-list-item>
+                        <p class="font-weight-black mr-3">{{subTitle}}: </p>
+                        <p> {{subContents[subTitle]}} </p>
+                    </v-list-item>
+                </v-list>
+            
+                <v-divider></v-divider>
+                <p>요놈 진짜 못생겼어요! ㅎㅎㅎ</p>
             </v-card-text>
-            <v-card-actions>
+            <v-card-actions class="d-flex justify-center">
               <v-spacer></v-spacer>
-              
+              <v-btn>수정</v-btn>
+              <v-btn>삭제</v-btn>
+              <v-btn>목록</v-btn>
             </v-card-actions>
           </v-card>
       </v-container>
@@ -34,6 +47,13 @@ export default {
     },
     data(){
         return {
+            subTitles:['견종', '날짜', '장소', '특징'],
+            subContents:{
+                '견종': '보스턴 테리어',
+                '날짜': '2020-08-06',
+                '장소': '대전광역시 유성구',
+                '특징': '못생김',
+            }
         }
     }
 }
--- frontend/src/page/lost/LostList.vue
+++ frontend/src/page/lost/LostList.vue
@@ -3,29 +3,16 @@
     <v-container >
       <form>
         <v-row class="d-flex align-center mx-auto">
-          <v-col cols="2" sm="2" md="2">
-            <v-select
-              v-model="category"
-              :items="items"
-              label="카테고리"
-              item-text="state"
-              item-value="abbr"
-              solo
-            ></v-select>
-          </v-col>
-          <v-col cols="8" sm="8" md="8">
+          <v-col></v-col>
+          <v-col class="d-flex">
             <v-text-field
               label="검색어를 입력하세요"
               v-model="searchText"
+              class="mx-2"
             ></v-text-field>
-          </v-col>
-          <v-col class="mb-4 pa-0" cols="2" sm="2" md="2">
             <v-btn large @click="search" type="submit">
               검색
             </v-btn>
-            <div class="v-messages theme--light">
-              <div class="v-messages__wrapper"></div>
-            </div>
           </v-col>
         </v-row>
       </form>
@@ -55,6 +42,9 @@
         </v-col>
       </v-row>
     </v-container>
+    <v-btn class="float-btn" color="success" fab x-large dark>
+      <v-icon>mdi-pencil</v-icon>
+    </v-btn>
   </div>
 </template>
 
@@ -64,6 +54,14 @@ export default {
   data(){
     return {
       cards: [
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
+        { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
         { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
         { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
         { title: 'Pre-fab homes', src: 'https://cdn.vuetifyjs.com/images/cards/house.jpg', flex: 3 },
@@ -80,5 +78,9 @@ export default {
 </script>
 
 <style>
-
+.float-btn {
+  position: fixed;
+  bottom: 5vh;
+  right: 5vw;
+}
 </style>
