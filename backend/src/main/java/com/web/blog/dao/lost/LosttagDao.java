032dab9f4090c4463d2f90ffcdc1096a65b717d9
김민주 <ynjq01910@naver.com>
2020-08-06 17:12:41 +0900

    Revert "[추가] S03P13B201-23 [+] Add LostController, LostDao, LosttagDao.java"
    This reverts commit dea01e476c4479cb6a37baa0c7faca6e239193c4.

--- backend/src/main/java/com/web/blog/dao/lost/LosttagDao.java
+++
@@ -1,9 +0,0 @@
-package com.web.blog.dao.lost;
-
-import com.web.blog.model.lost.Losttag;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface LosttagDao extends JpaRepository<Losttag, String> {
-    
-}
