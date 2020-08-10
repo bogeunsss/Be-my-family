9f97557fe0ab0f334795f2bce4cea9697e3b7328
길훈성 <ghs12222@naver.com>
2020-08-10 18:08:01 +0900

    [추가] S03P13B201-50 [+] 입양후기 추가 back-end

---
+++ backend/src/main/java/com/web/blog/controller/postscript/PostscriptCommentController.java
@@ -0,0 +1,43 @@
+package com.web.blog.controller.postscript;
+
+import java.io.File;
+import java.util.List;
+import java.util.Optional;
+
+import com.web.blog.dao.lost.LostDao;
+import com.web.blog.dao.lost.LosttagDao;
+import com.web.blog.model.BasicResponse;
+import com.web.blog.model.lost.Lost;
+import com.web.blog.model.lost.LostPic;
+import com.web.blog.model.lost.LostRequest;
+import com.web.blog.model.lost.Losttag;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestPart;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
+
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponse;
+import io.swagger.annotations.ApiResponses;
+
+
+@ApiResponses(value = { @ApiResponse(code = 401, message = "Unauthorized", response = BasicResponse.class),
+        @ApiResponse(code = 403, message = "Forbidden", response = BasicResponse.class),
+        @ApiResponse(code = 404, message = "Not Found", response = BasicResponse.class),
+        @ApiResponse(code = 500, message = "Failure", response = BasicResponse.class) })
+
+@CrossOrigin(origins = { "http://localhost:3000" })
+@RestController
+public class PostscriptCommentController {
+    
+}
