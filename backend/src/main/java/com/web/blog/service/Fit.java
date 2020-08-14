bf441507456d3fc9211b0891705f79eed9ffa195
김민주 <ynjq01910@naver.com>
2020-08-14 18:18:03 +0900

    [수정] S03P13B201-78 [+] 설문조사 바탕으로 적합 부적합 판별

--- backend/src/main/java/com/web/blog/service/Fit.java
+++ backend/src/main/java/com/web/blog/service/Fit.java
@@ -1,5 +1,6 @@
 package com.web.blog.service;
 
+import com.web.blog.model.care.Careboard;
 import com.web.blog.model.care.Survey;
 
 import org.springframework.stereotype.Service;
@@ -7,26 +8,44 @@ import org.springframework.stereotype.Service;
 @Service
 public class Fit {
 
+    //사용자 설문 기준 최소 적합 부적합 영역
     public int proper(Survey survey) {
 
         int no = 2;
         int result = 1;
 
-        //적합 부적합 영역
         if(!survey.getNation().equals("korea")) {
             result = no;
         } else if(survey.getPlace().equals("business")) {
+            result = no;
+        } else if(survey.getPresentanimal() >= 6) {
+            result = no;
+        } else if (survey.getFamilyunder() >= 2) {
+            result = no;
+        } else if (survey.getFamilymiddle() >= 3) {
+            result = no;
+        } else if (survey.getFamilyagree() == 0) {
+            result = no;
+        } else if (survey.getDissolution() == 2) {
+            result = no;
+        } else if (survey.getSickness() == 1) {
             result = no;
         }
 
         return result;
     }
 
-    //강아지 추천 영역
-    public String recommend () {
+    //설문 적합 사용자 기반으로 강아지 추천 영역
+    public Boolean recommend (Careboard dog, Survey survey) {
 
-        return " ";
+        Boolean ok = true;
+        Boolean result = false;
+
+        //강아지 kg
+        //강아지 종
+        //거주 형태
+
+        return result;
     }
-
 
 }
