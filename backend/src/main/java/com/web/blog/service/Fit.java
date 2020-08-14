548c88c09fd3c2f4537edb0addb1ea675ece78a2
김민주 <ynjq01910@naver.com>
2020-08-14 19:53:15 +0900

    [수정] S03P13B201-78 [+] 적합인 유저 강아지 추천 틀 구현

--- backend/src/main/java/com/web/blog/service/Fit.java
+++ backend/src/main/java/com/web/blog/service/Fit.java
@@ -8,17 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class Fit {
 
-    //사용자 설문 기준 최소 적합 부적합 영역
+    //  사용자 설문 기준 최소 적합 부적합 영역
     public int proper(Survey survey) {
 
         int no = 2;
         int result = 1;
 
-        if(!survey.getNation().equals("korea")) {
+        if  (!survey.getNation().equals("korea")) {
             result = no;
-        } else if(survey.getPlace().equals("business")) {
+        } else if  (survey.getPlace().equals("business")) {
             result = no;
-        } else if(survey.getPresentanimal() >= 6) {
+        } else if  (survey.getPresentanimal() >= 6) {
             result = no;
         } else if (survey.getFamilyunder() >= 2) {
             result = no;
@@ -35,15 +35,33 @@ public class Fit {
         return result;
     }
 
-    //설문 적합 사용자 기반으로 강아지 추천 영역
-    public Boolean recommend (Careboard dog, Survey survey) {
+    //  설문 적합 사용자 기반으로 강아지 추천 영역
+    public boolean recommend (Careboard dog, Survey survey) {
 
-        Boolean ok = true;
-        Boolean result = false;
+        boolean ok = true;
+        boolean no = false;
+        boolean result = false;
+ 
+        String house = survey.getHouse();
+        String kind = dog.getKindcd();
+        String weight = dog.getWeight();
+        int kg = Integer.parseInt(weight.replace("(Kg)", ""));
+        
+        if (house.equals("room") || house.equals("apt") || house.equals("villa")) {
+            //종
+            if(kind.contains("꼬똥 드 뚤레아") || kind.contains("말티즈") || kind.contains("퍼그") || kind.contains("토이 푸들") ||
+            kind.contains("포메라니안") || kind.contains("미니어쳐 푸들") || kind.contains("시츄") || kind.contains("요크셔 테리어") ||
+            kind.contains("치와와") || kind.contains("비숑 프리제")) {
+                result = ok;
+            }
+        } else {
 
-        //강아지 kg
-        //강아지 종
+        }
+
         //거주 형태
+        //거주 지역
+        //place
+
 
         return result;
     }
