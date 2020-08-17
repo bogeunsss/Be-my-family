3d30a60c2870dc9f7fe5efa8288ff31f02e13d3f
길훈성 <ghs12222@naver.com>
2020-08-17 18:05:03 +0900

    [수정] careboard model 수정

--- backend/src/main/java/com/web/blog/model/care/Careboard.java
+++ backend/src/main/java/com/web/blog/model/care/Careboard.java
@@ -17,34 +17,27 @@ import com.fasterxml.jackson.annotation.JsonInclude;
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class Careboard {
 
-    @Id
-    private String desertionno;
-
-    private String noticeedt;
-    private String popfile;
-    private String processstate;
-    private String sexcd;
-    private String neuteryn;
-    private String specialmark;
+    private String age;
+    private String careaddr;
     private String carenm;
     private String caretel;
-    private String careaddr;
-    private String orgnm;
     private String chargenm;
-    private String officetel;
-    private String noticecomment;
-    private String numofrows;
-    private String pageno;
-    private String totalcount;
-    private String resultcode;
-    private String resultmsg;
+    private String colorcd;
+    @Id
+    private String desertionno;
     private String filename;
     private String happendt;
     private String happenplace;
     private String kindcd;
-    private String colorcd;
-    private String age;
-    private String weight;
+    private String neuteryn;
+    private String noticeedt;
     private String noticeno;
     private String noticesdt;
+    private String officetel;
+    private String orgnm;
+    private String popfile;
+    private String processstate;
+    private String sexcd;
+    private String specialmark;
+    private String weight;
 }
