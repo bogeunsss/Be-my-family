cae4165ad293a7dbecd9da88256c221ed1a27582
길훈성 <ghs12222@naver.com>
2020-08-18 09:46:11 +0900

    [수정] 관심목록 리스트 수정

--- backend/src/main/java/com/web/blog/dao/care/InterestDao.java
+++ backend/src/main/java/com/web/blog/dao/care/InterestDao.java
@@ -15,12 +15,10 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 
 public interface InterestDao extends JpaRepository<Interest, String> {
-    // List<Interest> findByUidAndDesertionno(String uid, String desertionNo);
-    //아래도 써줘야 하는데 이름이 같아서 쓸 수 없음...
-    // Interest findByUidAndDesertionno(String uid, String desertionNo);
     @Transactional
     void deleteByInterestno(int interestno);
     Interest save(Interest interest);
     List<Interest> findByUid(String uid);
     Optional<Interest> findByUidAndDesertionno(String uid, String desertionno);
+    List<Interest> findByUidOrderByDesertionno(String uid);
 }
