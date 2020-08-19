26e677576baf70f828de297d933cb842bc6d5a5b
길훈성 <ghs12222@naver.com>
2020-08-19 11:18:32 +0900

    [수정] webconfig.java

--- backend/src/main/java/com/web/blog/config/WebConfig.java
+++ backend/src/main/java/com/web/blog/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://i3b201.p.ssafy.io/").allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH");
+                .allowedOrigins("http://i3b201.p.ssafy.io").allowedMethods("PUT", "DELETE", "POST", "GET", "PATCH");
     }
 
 }
