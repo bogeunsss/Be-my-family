67102ad67985feb8d6f10de25c2ead004dab6adb
최보근 <bogun11904@naver.com>
2020-08-20 16:08:34 +0900

    final work

--- frontend/src/page/lost/LostList.vue
+++ frontend/src/page/lost/LostList.vue
@@ -477,6 +477,7 @@ export default {
         }
       }).then(response => {
         console.log(response)
+        this.$reouter.go()
       }).catch(error => {
         console.log(error)
       })
